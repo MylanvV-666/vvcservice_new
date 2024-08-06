809df6cf7f6bf76d66f21c5c76e77eccaec49dc4
MylanvV-666 <12200295@student.pxl.be>
2024-08-06 16:14:39 +0200

    popup werkt

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "flex";
-    dots[slideIndex-1].className += " active";
-}
-
 let onDienstenPage = document.getElementById("diensten");
 let restUnactive1 = document.getElementsByClassName("actieflink")[0];
 let restUnactive2 = document.getElementsByClassName("actieflink")[1];
@@ -50,3 +21,12 @@ function dienstenUnactive() {
     dienstenlink1.style.color="#fff";
     dienstenlink2.style.color="#fff";
 }
+
+//popup
+let popup = document.getElementById("popup");
+document.getElementById("closepopup").addEventListener("click", closepopup);
+
+function closepopup() {
+    popup.style.display="none";
+}
+
